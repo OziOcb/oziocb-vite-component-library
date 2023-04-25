e3f93ddbf332850d0669b5ede4d71d2ec78e2845
Paul Ozyzniewski <paul.ozyzniewski@ignite.systems>
2023-04-25 15:11:14 +0100

    Clean up and create MyButton component

---
+++ src/components/MyButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button :class="['my-button', primary && 'my-button--primary']">
+    <slot></slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  primary?: boolean;
+}>();
+</script>
+
+<style scoped>
+.my-button {
+  background-color: #ada;
+}
+.my-button.my-button--primary {
+  background-color: #b00b55;
+}
+</style>
